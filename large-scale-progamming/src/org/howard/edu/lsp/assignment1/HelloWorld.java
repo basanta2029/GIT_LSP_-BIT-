57ac2bce85dc7c3f6d5f160f800aaf9c44a399ba
Basanta <basantabaral2300@gmail.com>
2024-02-02 22:19:46 -0500

    This is Assignment #1

---
+++ large-scale-progamming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+*  Name: Basanta Baral
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
